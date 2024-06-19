3f8adfc74a8d7b14210fc421627b46a6188506d3
AndreaSchiariti <schiariti.andrea@gmail.com>
2024-06-19 02:50:06 +0200

    Implementato lavoro in serie di pagine notizia, completato html e css di apex e the sims 4

--- news/nfs_notizie.js
+++ news/nfs_notizie.js
@@ -27,3 +27,72 @@ window.addEventListener("scroll", () => {
 });
 
 /* Fine Sticky Navbar */
+
+/* INIZIO Sezione delle notizie */
+
+/* Creo una funzione che mi genera le notizie prendendo le informazioni 
+da un API (in questo caso un localStorage) */
+
+const griglia = document.getElementById("griglia-notizie");
+const notizieNelLocale = localStorage.getItem("notizie-ea");
+const oggettoNotizie = JSON.parse(notizieNelLocale);
+
+function generaCardNotizia(notizia) {
+  /* Creo il section della card */
+  const section = document.createElement("section");
+  section.classList.add("news-card");
+  griglia.appendChild(section);
+
+  /* Creo il link della card 
+  - DENTRO section */
+  const a = document.createElement("a");
+  a.href = "#";
+  section.appendChild(a);
+
+  /* Creo l'immagine della card e un contenitore per la parte testuale
+  - DENTRO a */
+
+  const immagineNotizia = document.createElement("img");
+  immagineNotizia.src = notizia.imagesrc;
+  immagineNotizia.alt = notizia.imagealt;
+  a.appendChild(immagineNotizia);
+
+  const divNotizie = document.createElement("div");
+  a.appendChild(divNotizie);
+
+  /* Creo i vari sottogruppi delle notizie.
+  - DENTRO divNotizie */
+
+  const divAutoreEData = document.createElement("div");
+  divAutoreEData.classList.add("autore-e-data");
+  divNotizie.appendChild(divAutoreEData);
+
+  /*Creo autore e data della notizia
+    - DENTRO divAutoreEData */
+  const paragrafoAutore = document.createElement("p");
+  paragrafoAutore.classList.add("news-info", "pink");
+  paragrafoAutore.innerHTML = notizia.autore;
+  divAutoreEData.appendChild(paragrafoAutore);
+
+  const paragrafoData = document.createElement("p");
+  paragrafoData.classList.add("news-info", "black");
+  paragrafoData.innerHTML = notizia.data;
+  divAutoreEData.appendChild(paragrafoData);
+
+  /* torno
+  - DENTRO divNotizie */
+  const hTitolo = document.createElement("h3");
+  hTitolo.innerHTML = notizia.titolo;
+  divNotizie.appendChild(hTitolo);
+
+  const paragrafoDidascalia = document.createElement("p");
+  paragrafoDidascalia.classList.add("articolo-della-news");
+  paragrafoDidascalia.innerHTML = notizia.didascalia;
+  divNotizie.appendChild(paragrafoDidascalia);
+}
+
+for (let i = 0; i < oggettoNotizie.length; i++) {
+  generaCardNotizia(oggettoNotizie[i]);
+}
+
+/* FINE Sezione delle notizie */
--- news/the_sims_4_notizie.js
+++ news/the_sims_4_notizie.js
@@ -27,3 +27,49 @@ window.addEventListener("scroll", () => {
 });
 
 /* Fine Sticky Navbar */
+
+const griglia = document.getElementById("griglia-notizie");
+const notizieNelLocale = localStorage.getItem("notizie-ea");
+const oggettoNotizie = JSON.parse(notizieNelLocale);
+
+function generaCardNotizia(notizia) {
+  /* Creo il section della card */
+  const section = document.createElement("section");
+  section.classList.add("news-card");
+  griglia.appendChild(section);
+
+  /* Creo il link della card 
+  - DENTRO section */
+  const a = document.createElement("a");
+  a.href = "#";
+  section.appendChild(a);
+
+  /* Creo l'immagine della card e un contenitore per la parte testuale
+  - DENTRO a */
+
+  const immagineNotizia = document.createElement("img");
+  immagineNotizia.src = notizia.imagesrc;
+  immagineNotizia.alt = notizia.imagealt;
+  a.appendChild(immagineNotizia);
+
+  const divNotizie = document.createElement("div");
+  a.appendChild(divNotizie);
+
+  const hTitolo = document.createElement("h3");
+  hTitolo.innerHTML = notizia.titolo;
+  divNotizie.appendChild(hTitolo);
+
+  const paragrafoDidascalia = document.createElement("p");
+  paragrafoDidascalia.classList.add("articolo-della-news");
+  paragrafoDidascalia.innerHTML = notizia.didascalia;
+  divNotizie.appendChild(paragrafoDidascalia);
+
+  const strisciaRossa = document.createElement("div");
+  strisciaRossa.classList.add("striscia-rossa")
+  section.appendChild(strisciaRossa)
+}
+
+
+for (let i = 0; i < oggettoNotizie.length; i++) {
+  generaCardNotizia(oggettoNotizie[i]);
+}
